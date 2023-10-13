538eee2d06455b82b6e4876b23d89126d15d02d5
kimhyun <forhn37@gmail.com>
2023-10-13 12:12:26 +0900

    서버 구축 중

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-let http = require('http');
+const http = require("http");
+const fs = require('fs');
+// 기본 서버 구축
+http.createServer(function(request, response) {
+  // mainpage
+  if(request.url === "/") {
+    console.log(request.url);
+    fs.readFile("index.html",function(err, data) {
+      if(err) {
+        console.err('에러입니다.');
+      } else {
+        response.writeHead(200, {'Content-Type': 'text/html'})
+        response.end(data);
+      }
+      })
+  }
+
+  if(request.url === "/sub") {
+    console.log(request.url);
+    fs.readFile("index.html",function(err,data){
+      if(err){
+        console.error('에러 입니다.');
+      } else {        
+        response.writeHead(200, {'Content-Type': 'text/html'})
+        response.end(data);
+      }
+    }
+    }).listen(5050)

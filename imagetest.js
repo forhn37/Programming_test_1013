bd3e354ae008c269783823aab794764994e4d347
kimhyun <forhn37@gmail.com>
2023-10-15 12:49:29 +0900

    filedir 확인

--- imagetest.js
+++ imagetest.js
@@ -1,1 +1,11 @@
-console.log("helloworld")
+const fs = require("fs");
+let arr =[];
+fs.readdirSync("./pokemon", function(err, filelist) {
+  if(err) {
+    console.log("err")
+  } 
+  for(let i=0; i<filelist.length; i++) {
+    arr[i] = filelist[i];
+  }
+})
+console.log(arr);
